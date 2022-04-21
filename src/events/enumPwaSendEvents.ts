24eda9f4a2742df4073347c4b56f2a08850f61ff
Rahul Singh <rahul@autuskey.com>
2022-04-21 18:51:06 +0530

    Event changes

--- src/events/enumPwaSendEvents.ts
+++ src/events/enumPwaSendEvents.ts
@@ -3,4 +3,5 @@ export enum EnumPwaSendEvent {
   PS_BIKE_HEARTBEAT_UPDATE = "psBikeHeartbeatUpdate",
   PS_BIKE_STATUS_UPDATE = "psBikeStatusUpdate",
   PS_BIKE_DISPLAY_UPDATE = "psBikeDisplayUpdate",
+  PS_BIKE_LOCK_UPDATE = "psBikeLockUpdate",
 }
