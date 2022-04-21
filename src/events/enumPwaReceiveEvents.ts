8922d7034729952c6f1b43d80d5539e175412e72
Rahul Singh <rahul@autuskey.com>
2022-04-21 12:31:16 +0530

    Minor Fixes

--- src/events/enumPwaReceiveEvents.ts
+++ src/events/enumPwaReceiveEvents.ts
@@ -1,7 +1,10 @@
-enum EnumPwaReceiveEvent {
-  PS_BIKE_SETTINGS_UPDATE = 'psBikeSettingsUpdate',
-  PS_BIKE_HEARTBEAT_UPDATE = 'psBikeHeartbeatUpdate',
-  PS_BIKE_STATUS_UPDATE = 'psBikeStatusUpdate',
-  PS_BIKE_DISPLAY_UPDATE = 'psBikeDisplayUpdate',
+export enum EnumPwaReceiveEvent {
+  PR_BIKE_CONNECT_STATUS = "prBikeConnectStatus",
+  PR_BIKE_SETTINGS_UPDATE = "prBikeSettingsUpdate",
+  PR_BIKE_HEARTBEAT_UPDATE = "prBikeHeartbeatUpdate",
+  PR_BIKE_STATUS_UPDATE = "prBikeStatusUpdate",
+  PR_BIKE_DISPLAY_UPDATE = "prBikeDisplayUpdate",
+  PR_BIKE_DEBUG_UPDATE = "prBikeDebugUpdate",
+  PR_BIKE_ERROR_UPDATE = "prBikeErrorUpdate",
+ 
 }
-export default EnumPwaReceiveEvent;
--- src/events/enumPwaSendEvents.ts
+++ src/events/enumPwaSendEvents.ts
@@ -1,10 +1,6 @@
-enum EnumPwaSendEvent {
-  PR_BIKE_CONNECT_STATUS = 'prBikeConnectStatus',
-  PR_BIKE_SETTINGS_UPDATE = 'prBikeSettingsUpdate',
-  PR_BIKE_HEARTBEAT_UPDATE = 'prBikeHeartbeatUpdate',
-  PR_BIKE_STATUS_UPDATE = 'prBikeStatusUpdate',
-  PR_BIKE_DISPLAY_UPDATE = 'prBikeDisplayUpdate',
-  PR_BIKE_DEBUG_UPDATE = 'prBikeDebugUpdate',
-  PR_BIKE_ERROR_UPDATE = 'prBikeErrorUpdate',
+export enum EnumPwaSendEvent {
+  PS_BIKE_SETTINGS_UPDATE = "psBikeSettingsUpdate",
+  PS_BIKE_HEARTBEAT_UPDATE = "psBikeHeartbeatUpdate",
+  PS_BIKE_STATUS_UPDATE = "psBikeStatusUpdate",
+  PS_BIKE_DISPLAY_UPDATE = "psBikeDisplayUpdate",
 }
-export default EnumPwaSendEvent;
