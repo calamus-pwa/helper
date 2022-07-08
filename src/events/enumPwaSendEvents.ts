5f563ad6be7650ed9c39926584e656f18eecae76
Rahul Singh <rahul@autuskey.com>
2022-07-08 15:04:44 +0530

    Added send events

--- src/events/enumPwaSendEvents.ts
+++ src/events/enumPwaSendEvents.ts
@@ -4,4 +4,6 @@ export enum EnumPwaSendEvent {
   PS_BIKE_STATUS_UPDATE = "psBikeStatusUpdate",
   PS_BIKE_DISPLAY_UPDATE = "psBikeDisplayUpdate",
   PS_BIKE_LOCK_UPDATE = "psBikeLockUpdate",
+  PS_BIKE_TO_LOCATION_UPDATE = "psBikeToLocationUpdate",
+  PS_BIKE_LOST_UPDATE = "psBikeLostUpdate",
 }
